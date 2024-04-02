b60a7ff644896c69d9bcc2a2141035cfa4b2903a
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-04-02 12:27:13 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("welcome to BAckend");
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+    } catch (error) {
+        console.error("error", error)
+    }
+})()
