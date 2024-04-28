03ce6b8593509efe204d5512b5dfd363a9af630e
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-04-28 15:59:35 +0530

    updated

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import  express, { urlencoded }  from "express"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+
+const app=express()
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.use(cookieParser())
+export default app
